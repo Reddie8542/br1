f9d32d405d3f1c3f1f3a721f4af88a07fcd53d5e
Reddie8542 <bruno.hiram@tutanota.com>
2021-06-22 07:44:46 -0500

    Replace network links with anchor tags

--- src/app/home/social/social.component.ts
+++ src/app/home/social/social.component.ts
@@ -15,16 +15,20 @@ interface Network {
 export class SocialComponent {
   networks: Network[] = [
     { name: 'GitHub', url: 'https://github.com/Reddie8542', icon: 'code', tooltip: '' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/br1-alva/', icon: 'business_center', tooltip: '' },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/br1-alva/',
+      icon: 'business_center',
+      tooltip: '',
+    },
     { name: 'Facebook', url: 'https://www.facebook.com/bruno.alva.3/', icon: 'facebook', tooltip: '' },
     { name: 'Twitter', url: 'https://twitter.com/Reddie8542', icon: 'flutter_dash', tooltip: '' },
     { name: 'Instagram', url: 'https://www.instagram.com/br1alva/', icon: 'camera', tooltip: '' },
-    { name: 'Discord', url: null, icon: 'videogame_asset', tooltip: 'Reddie#8542' },
+    {
+      name: 'Discord',
+      url: 'https://discord.com/',
+      icon: 'videogame_asset',
+      tooltip: 'Reddie#8542',
+    },
   ];
-
-  open(url: string | null | undefined) {
-    if (url != null) {
-      window.open(url, '_blank');
-    }
-  }
 }
