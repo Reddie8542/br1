1e9cfd8253a6e49ce07bd2eb5ceda75bba676a63
Reddie8542 <bruno.hiram@tutanota.com>
2021-06-26 13:34:06 -0500

    Remove unused import

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
-import { delay } from 'rxjs/operators';
 import { AuthService } from 'src/services/auth.service';
 import { adminTab, HomeComponent } from './home.component';
 
